ffcab708a8754e42df122b1770c2764046904b36
ju-zp <jhuntington@tutanota.com>
2019-01-11 15:57:38 +0000

    added .gitignor

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+var five = require("johnny-five");
+const app = express()
+const port = 3002
+
+
+var board = new five.Board();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    req.connection.setNoDelay(true)
+    next();
+  });
+
+board.on("ready", function() {
+    let led = new five.Led.RGB({
+        pins: {
+            red: 9,
+            green: 10,
+            blue: 11
+        },
+        anode: true
+    })
+    led.on()
+    // var red = new five.Led(9);
+    // var green = new five.Led(10);
+    // var blue = new five.Led(11);
+    // green.on();
+    // blue.on();
+    // red.on();
+    app.get('/switchOn', (req, res) => {
+        led.off()
+        console.log("hello")
+        res.send("yes")
+        // console.log(res)
+    });
+
+    app.get('/switchOff', (req, res) => {
+        console.log("goodbye")
+        
+        // console.log(res)
+        led.on()
+        res.send("yes")
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
